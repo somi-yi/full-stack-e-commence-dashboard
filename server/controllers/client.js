1ee3d9eb7a8d12aec98d85d8cf63433f9b560bea
somi-yi <somiyi@163.com>
2024-04-27 16:58:02 +0100

    change on controllers file

--- server/controllers/client.js
+++ server/controllers/client.js
@@ -1,7 +1,7 @@
-import Product from "../../models/Product.js";
-import ProductStat from "../../models/ProductStat.js";
-import User from "../../models/User.js";
-import Transaction from "../../models/Transaction.js";
+import Product from "../models/Product.js";
+import ProductStat from "../models/ProductStat.js";
+import User from "../models/User.js";
+import Transaction from "../models/Transaction.js";
 import getCountryIso3 from "country-iso-2-to-3";
 
 export const getProducts = async (req, res) => {
--- server/controllers/general.js
+++ server/controllers/general.js
@@ -1,6 +1,6 @@
-import User from "../../models/User.js";
-import OverallStat from "../../models/OverallStat.js";
-import Transaction from "../../models/Transaction.js";
+import User from "../models/User.js";
+import OverallStat from "../models/OverallStat.js";
+import Transaction from "../models/Transaction.js";
 
 export const getUser = async (req, res) => {
   const { id } = req.params;
